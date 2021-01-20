f9edb7f5af201a307265bf5cb1b82c87e51cfbf9
Miguel <miguel.casado@iesnervion.es>
2021-01-20 20:46:44 +0100

    Fragments added

--- app/src/main/java/es/iesnervion/ejemplofragments2021/MainActivity.java
+++ app/src/main/java/es/iesnervion/ejemplofragments2021/MainActivity.java
@@ -3,12 +3,25 @@ package es.iesnervion.ejemplofragments2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        View mainContainer;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainContainer = findViewById(R.id.mainContainer);
+
+        //If the layout that has been loaded is the one for small screens
+        if (mainContainer!=null){
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.mainContainer, NavigationFragment.class, null)
+                    .commit();
+        }
     }
 }
